a47fa9c3694ebf9b534f2adbfa06a65c7f8665cf
Roman Joost <roman@bromeco.de>
2017-08-17 19:39:05 +1000

    prepare notmuch environment

--- src/Test/Tasty/Tmux.hs
+++ src/Test/Tasty/Tmux.hs
@@ -14,13 +14,23 @@
 -- You should have received a copy of the GNU Affero General Public License
 -- along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+{-# LANGUAGE OverloadedStrings #-}
+
 module Test.Tasty.Tmux where
 
+import Data.List (isInfixOf)
+import Control.Monad (forM_)
+import qualified Data.Text as T
+import System.IO.Temp (withSystemTempDirectory)
+import System.Directory (createDirectory, copyFile, listDirectory)
+import Data.Semigroup ((<>))
 import GHC.MVar (MVar)
 import Control.Concurrent (newEmptyMVar, forkIO, putMVar, takeMVar, killThread, threadDelay)
 import Control.Exception (bracket)
 
-import System.Process (callProcess, readProcess, CreateProcess(..), CmdSpec(..), StdStream(..))
+import System.Process
+       (shell, callProcess, readProcess, readCreateProcess, CreateProcess(..),
+        CmdSpec(..), StdStream(..))
 import System.Directory (getCurrentDirectory, removeFile)
 import Test.Tasty (TestTree, testGroup)
 import Test.Tasty.Golden (goldenVsFile)
@@ -37,12 +47,17 @@ testMakesHardcopy = goldenVsFile "does not crash" "test/data/test.golden" "/tmp/
 
 smokeTest :: IO ()
 smokeTest = do
-  _ <- setUp =<< getCurrentDirectory
-  callProcess "tmux" $ communicateSessionArgs ++ ["j", "j", "Enter"]
-  out <- readProcess "tmux" hardcopyArgs []
-  print out
-  callProcess "tmux" $ savebufferArgs "/tmp/testoutput"
-  teardown
+  withSystemTempDirectory "purebredtest" $ \fp -> do
+    testmdir <- prepareMaildir fp
+    cfg <- prepareNotmuchCfg fp testmdir
+    out <- prepareNotmuch cfg
+    print out
+    _ <- setUp testmdir
+    callProcess "tmux" $ communicateSessionArgs ++ ["j", "j", "Enter"]
+    out <- readProcess "tmux" hardcopyArgs []
+    print out
+    callProcess "tmux" $ savebufferArgs "/tmp/testoutput"
+    teardown
 
 waitReady :: MVar String -> IO ()
 waitReady baton = do
@@ -56,8 +71,21 @@ waitReady baton = do
 applicationReadySignal :: ByteString
 applicationReadySignal = pack "READY=1"
 
-tmuxSessionArgs :: String -> [String]
-tmuxSessionArgs c = ["new-session", "-P", "-F", "#{session_name}:#{command_name}-#{buffer_sample}", "-x", "95", "-y", "56", "-d", "-s", "purebredtest", "-n", "purebred", "-c", c, "purebred"]
+tmuxSessionArgs :: FilePath -> [String]
+tmuxSessionArgs cfg =
+    [ "new-session"
+    , "-x"
+    , "95"
+    , "-y"
+    , "56"
+    , "-d"
+    , "-s"
+    , "purebredtest"
+    , "-n"
+    , "purebred"
+    , "purebred"
+    , "--database"
+    , cfg]
 
 communicateSessionArgs :: [String]
 communicateSessionArgs = words "send-keys -t purebredtest"
@@ -69,9 +97,18 @@ savebufferArgs :: FilePath -> [String]
 savebufferArgs hardcopypath =
     ["save-buffer", "-b", "purebredcapture", hardcopypath]
 
+checkSessionStarted :: IO ()
+checkSessionStarted = do
+    out <- readProcess "tmux" ["list-sessions"] []
+    print out
+    if "purebredtest" `isInfixOf` out
+        then pure ()
+        else error "no session created"
+
 teardown :: IO ()
 teardown = do
   -- quit the application
   callProcess "tmux" $ communicateSessionArgs ++ (words "Esc Enter")
   -- XXX make sure it's dead
   callProcess "tmux" $ words "unlink-window -k -t purebredtest"
+
